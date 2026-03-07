4e095d35ddc677b028cf675a0ce36d7f3056c12a
Jay Turner <jaynicholasturner@gmail.com>
2026-03-07 15:56:58 +0000

    Flakes

--- configuration.nix
+++ configuration.nix
@@ -19,7 +19,7 @@
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
   boot.initrd.luks.devices."luks-d5a84443-b5a5-485b-848e-8c50df120425".device = "/dev/disk/by-uuid/d5a84443-b5a5-485b-848e-8c50df120425";
-  networking.hostName = "nixos"; # Define your hostname.
+  networking.hostName = "jay-framework"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
@@ -110,6 +110,7 @@
 
   # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
