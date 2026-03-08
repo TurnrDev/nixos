d7a92c52ead423122b3f9b7c2ac12c9ccc5df17b
Jay Turner <jaynicholasturner@gmail.com>
2026-03-08 13:01:13 +0000

    Configures display profiles with Kanshi
    Expands the user's home configuration to include settings for the Kanshi dynamic display manager. Defines distinct profiles for the internal laptop display and a docked setup that utilizes an external monitor, enabling automatic display arrangement based on connected peripherals.

--- hosts/jay-framework/home.nix
+++ hosts/jay-framework/home.nix
@@ -7,4 +7,41 @@
 
   home.username = "jay";
   home.homeDirectory = "/home/jay";
+
+  services.kanshi = {
+    enable = true;
+    settings = [
+      {
+        profile.name = "internal";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            scale = 1.0;
+            mode = "2256x1504@60.00Hz";
+            position = "0,0";
+          }
+        ];
+      }
+      {
+        profile.name = "docked";
+        profile.outputs = [
+          {
+            criteria = "eDP-1";
+            status = "enable";
+            scale = 1.0;
+            mode = "2256x1504@60.00Hz";
+            position = "0,1440";
+          }
+          {
+            criteria = "Samsung Electric Company LC49G95T H1AK500000";
+            status = "enable";
+            scale = 1.0;
+            mode = "5120x1440@59.98Hz";
+            position = "0,0";
+          }
+        ];
+      }
+    ];
+  };
 }
